4c7e999cc2f5bd3e4cb5f69a9c23a8e913a40d35
AlexLegovich <34690224+AlexLegovich@users.noreply.github.com>
2022-11-10 21:59:57 +0200

    Add files via upload

---
+++ popups.js
@@ -0,0 +1,79 @@
+$('.hover-1').on('click', function(e){
+    e.preventDefault();
+    $('.popup-1, .popup__close').toggleClass('open');
+    $('body').toggleClass('lock');
+
+});
+
+$('.hover-2').on('click', function(e){
+    e.preventDefault();
+    $('.popup-2, .popup__close').toggleClass('open');
+    $('body').toggleClass('lock');
+
+});
+
+$('.hover-3').on('click', function(e){
+    e.preventDefault();
+    $('.popup-3, .popup__close').toggleClass('open');
+    $('body').toggleClass('lock');
+
+});
+$('.hover-4').on('click', function(e){
+    e.preventDefault();
+    $('.popup-4, .popup__close').toggleClass('open');
+    $('body').toggleClass('lock');
+
+});
+$('.hover-5').on('click', function(e){
+    e.preventDefault();
+    $('.popup-5, .popup__close').toggleClass('open');
+    $('body').toggleClass('lock');
+
+});
+$('.hover-6').on('click', function(e){
+    e.preventDefault();
+    $('.popup-6, .popup__close').toggleClass('open');
+    $('body').toggleClass('lock');
+
+});
+
+$('.hover-7').on('click', function(e){
+    e.preventDefault();
+    $('.popup-7, .popup__close').toggleClass('open');
+    $('body').toggleClass('lock');
+
+});
+
+$('.hover-8').on('click', function(e){
+    e.preventDefault();
+    $('.popup-8, .popup__close').toggleClass('open');
+    $('body').toggleClass('lock');
+
+});
+
+$('.hover-9').on('click', function(e){
+    e.preventDefault();
+    $('.popup-9, .popup__close').toggleClass('open');
+    $('body').toggleClass('lock');
+
+});
+$('.hover-10').on('click', function(e){
+    e.preventDefault();
+    $('.popup-10, .popup__close').toggleClass('open');
+    $('body').toggleClass('lock');
+
+});
+
+$('.hover-11').on('click', function(e){
+    e.preventDefault();
+    $('.popup-11, .popup__close').toggleClass('open');
+    $('body').toggleClass('lock');
+
+});
+$('.hover-12').on('click', function(e){
+    e.preventDefault();
+    $('.popup-12, .popup__close').toggleClass('open');
+    $('body').toggleClass('lock');
+
+});
+
